ea605f7ced744357491f7c4247269fdd9fffb4f9
CREATIVA\educacion <edgar13155@gmail.com>
2017-11-29 16:49:22 -0600

    Con login

---
+++ app/controllers/UserController.java
@@ -0,0 +1,7 @@
+package controllers;
+
+/**
+ * Created by educacion on 29/11/2017.
+ */
+public class UserController {
+}
---
+++ app/models/User.java
@@ -0,0 +1,7 @@
+package models;
+
+/**
+ * Created by educacion on 29/11/2017.
+ */
+public class User {
+}
